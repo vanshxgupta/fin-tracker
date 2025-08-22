0e13a85c61da13e54d60b6dc475317e3f1554bbb
Vansh <vanshgupta987654321@gmail.com>
2025-08-22 23:58:04 +0530

    created routes in app.jsx

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,14 +1,47 @@
 import { useState } from 'react'
 import React from 'react'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <div className='bg-amber-200'>App</div>
-    </>
+    <div>
+      <Router>
+        <Routes>
+         <Route path="/" element={<Root/>}/>
+         <Route path="/login" element={<Login/>}/>
+         <Route path="/signup" element={<SignUp/>}/>
+         <Route path="/dashboard" element={<Home/>}/>
+         <Route path="/income" element={<Income/>}/>
+         <Route path="/expense" element={<Expense/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+const Root=()=>{
+  //if token exists in localstorage
+  const isauthenticated=localStorage.getItem("token")
+
+  //redirect to dashboard if isauthenticated,
+  return isauthenticated?
+  (<Navigate to="/dashboard"/>)
+  :
+  (<Navigate to="/login"/>);
+
+}
