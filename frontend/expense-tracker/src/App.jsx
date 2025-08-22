9c6ff82fdbb4cfa0155d752b1f9fd76d329fc50e
Vansh <vanshgupta987654321@gmail.com>
2025-08-22 23:28:17 +0530

    fonts,tailwind config

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div>App</div>
+     <div className='bg-amber-200'>App</div>
     </>
   )
 }
