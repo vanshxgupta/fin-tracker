cc6c91382d3d1155a204c50fa7dc1471699377e4
Vansh <vanshgupta987654321@gmail.com>
2025-08-26 17:52:27 +0530

    Add database connection and server setup with routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,44 @@
-require("dotenv").config();
-const express=require("express");
-const cors=require("cors");
-const path=require("path");
+require("dotenv").config()
+const cors = require("cors")
+const path = require("path")
 
-const app=express();
+const express = require("express")
+const app = express()
+
+const connectDB = require("./config/db.js")
+const authRoutes = require("./routes/authRoutes.js")
+const incomeRoutes = require("./routes/incomeRoutes.js")
+const expenseRoutes = require("./routes/expenseRoutes.js")
+const dashboardRoutes = require("./routes/dashboardRoutes.js")
+
+
+// Middleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        // This line tells your server who is allowed to make requests to it (like sending or getting data).
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-type", "Authorization"],
+    })
+)
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
