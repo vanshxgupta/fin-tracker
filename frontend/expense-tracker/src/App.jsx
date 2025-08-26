05257a6053d8ad6025cf0e8d6e5c990d3ae4b61c
Vansh <vanshgupta987654321@gmail.com>
2025-08-26 19:56:05 +0530

    comments and route

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,47 +1,67 @@
-import { useState } from 'react'
 import React from 'react'
-import Login from './pages/Auth/Login'
-import SignUp from './pages/Auth/SignUp'
-import Home from './pages/Dashboard/Home'
-import Income from './pages/Dashboard/Income'
-import Expense from './pages/Dashboard/Expense'
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
-} from "react-router-dom"
+  Navigate,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Login from "../src/pages/auth/Login";
+import SignUp from "../src/pages/auth/SignUp";
+import Home from "../src/pages/Dashboard/Home";
+import Income from "../src/pages/Dashboard/Income";
+import Expense from "../src/pages/Dashboard/Expense";
+import UserProvider from './context/userContext';
+import {Toaster} from "react-hot-toast"
 
+const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-         <Route path="/" element={<Root/>}/>
-         <Route path="/login" element={<Login/>}/>
-         <Route path="/signup" element={<SignUp/>}/>
-         <Route path="/dashboard" element={<Home/>}/>
-         <Route path="/income" element={<Income/>}/>
-         <Route path="/expense" element={<Expense/>}/>
-        </Routes>
-      </Router>
-    </div>
+    <UserProvider>
+      <div className='text-xl font-medium text-black'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signUp" exact element={<SignUp />} />
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/income" exact element={<Income />} />
+            <Route path="/expense" exact element={<Expense />} />
+          </Routes>
+        </Router>
+      </div>
+
+      <Toaster
+        toastOption={{
+          className: "",
+          style: {
+            fontSize: "13px"
+          },
+        }}
+      />
+    </UserProvider>
   )
+}
+
+const Root = () => {
+  // check if token exist in localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // redirect to dashboard if login otherwise to login
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
 }
 
 export default App
 
-const Root=()=>{
-  //if token exists in localstorage
-  const isauthenticated=localStorage.getItem("token")
 
-  //redirect to dashboard if isauthenticated,
-  return isauthenticated?
-  (<Navigate to="/dashboard"/>)
-  :
-  (<Navigate to="/login"/>);
+// Router (or BrowserRouter)
+// Wraps your entire app.
+// Enables client-side routing (changing pages without refreshing).
+// Tracks the URL in the address bar and shows the correct page/component.
 
-}
+// Routes
+// A container for all your Routes.
+// Think of it as a switchboard: it reads the current URL and matches it to a Route.
+
+// Route
+// Defines a URL path and which component should render when that path is visited.
