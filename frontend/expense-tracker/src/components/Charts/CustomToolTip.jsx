bc7e5e262d0a98dba8d2a6f82efee0e88f234471
Vansh <vanshgupta987654321@gmail.com>
2025-08-27 06:59:25 +0530

    components of charts and tool tip

---
+++ frontend/expense-tracker/src/components/Charts/CustomToolTip.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CustomToolTip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className='bg-white shadow-md rounded-lg p-2 border border-gray-300'>
+                <p className='text-xs font-semibold text-purple-800 mb-1'>{payload[0].name}</p>
+                <p className='text-sm text-gray-600'>
+                    Amount: <span className='text-sm font-medium text-gray-900'>
+                        ₹{payload[0].value}
+                    </span>
+                </p>
+            </div>
+        )
+    }
+    return null;
+}
+
+export default CustomToolTip
