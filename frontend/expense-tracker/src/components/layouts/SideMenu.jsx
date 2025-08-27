d1f6245aecf117970254103bd7256ffdfa84b6c8
Vansh <vanshgupta987654321@gmail.com>
2025-08-27 12:22:08 +0530

    Fix border color and text color for active menu item in SideMenu component

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -23,7 +23,7 @@ const SideMenu = ({ activeMenu }) => {
   }
 
   return (
-    <div className='w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20'>
+    <div className='w-64 h-[calc(100vh-61px)] bg-white border-r border-black p-5 sticky top-[61px] z-20'>
       <div className='flex flex-col items-center justify-center gap-3 mt-3 mb-7'>
         {user?.profileImageUrl ? (
           <img
@@ -45,7 +45,7 @@ const SideMenu = ({ activeMenu }) => {
       {SIDE_MENU_DATA.map((item, index) => (
         <button
           key={`menu_${index}`}
-          className={`w-full flex items-center gap-4 text-[15px] ${activeMenu === item.label ? "text-white bg-primary" : "text-black"} py-3 px-6 rounded-lg mb-3 cursor-pointer active:opacity-70 ${activeMenu===item.label ? "" : "hover:bg-purple-100"}`}
+          className={`w-full flex items-center gap-4 text-[15px] ${activeMenu === item.label ? "text-black bg-primary" : "text-black"} py-3 px-6 rounded-lg mb-3 cursor-pointer active:opacity-70 ${activeMenu===item.label ? "" : "hover:bg-purple-100"}`}
           onClick={() => handleClick(item.path)}
         >
           <item.icon className='text-xl' />
